7f4b74e3d4eaf4882018209aee34d4d01c057589
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-09-27 07:26:46 +0530

    change

--- First-java/src/com/gaurab/EvenOdd.java
+++ First-java/src/com/gaurab/EvenOdd.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class EvenOdd {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the number: ");
 		int num = scn.nextInt();
 		if (num % 2 == 0) {
 			System.out.println("Even");
