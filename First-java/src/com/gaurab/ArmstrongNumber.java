686d041fd92243e43ef8a13f6a5791d68a1b0804
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-09-27 09:47:55 +0530

    hh

---
+++ First-java/src/com/gaurab/ArmstrongNumber.java
@@ -0,0 +1,14 @@
+package com.gaurab;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int num1 = scn.nextInt();
+		System.out.println("Enter the second number: ");
+		int num2 = scn.nextInt();
+	}
+
+}
