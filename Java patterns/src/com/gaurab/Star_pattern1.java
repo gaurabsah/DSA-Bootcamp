4cb715f64060d27e731ce540e2dc770b73760dbf
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-09-30 16:44:38 +0530

    star pattern-1

---
+++ Java patterns/src/com/gaurab/Star_pattern1.java
@@ -0,0 +1,23 @@
+package com.gaurab;
+
+public class Star_pattern1 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("------------------------------");
+//reverse of above
+		
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 4; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
