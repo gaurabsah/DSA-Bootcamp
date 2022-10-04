eb95863e77d945e69991d8f0deab4a5d1e72aef4
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-10-04 09:40:52 +0530

    diagonal pattern 1

---
+++ Java patterns/src/com/gaurab/Star_pattern9.java
@@ -0,0 +1,17 @@
+package com.gaurab;
+
+public class Star_pattern9 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (i >= 2 && j <= i - 1) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
