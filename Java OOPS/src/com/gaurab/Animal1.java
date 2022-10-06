3c67bf077f443886d2a123efaa169d213e2605bb
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-10-06 15:26:58 +0530

    initializing variable using reference variable

---
+++ Java OOPS/src/com/gaurab/Animal1.java
@@ -0,0 +1,15 @@
+package com.gaurab;
+
+public class Animal1 {
+	int age;
+	String color;
+
+	public static void main(String[] args) {
+		Animal1 aa = new Animal1();
+		aa.age = 10;
+		aa.color = "black";
+
+		System.out.println(aa.age + " " + aa.color);
+	}
+
+}
