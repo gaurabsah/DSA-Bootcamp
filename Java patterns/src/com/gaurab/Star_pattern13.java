29473913d1ee1b8580cad8ea81f1bb72e9ab8702
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-10-06 09:34:12 +0530

    two diagonal intersecting "X"

---
+++ Java patterns/src/com/gaurab/Star_pattern13.java
@@ -0,0 +1,18 @@
+package com.gaurab;
+
+public class Star_pattern13 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+//				System.out.print("*");
+				if (i == j || i + j == 5 - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
