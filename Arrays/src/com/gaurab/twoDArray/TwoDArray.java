d8a2362d17791bc9fe282bcbb9c38c77c6120369
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-06 16:28:16 +0530

    2D Array

---
+++ Arrays/src/com/gaurab/twoDArray/TwoDArray.java
@@ -0,0 +1,38 @@
+package com.gaurab.twoDArray;
+
+import java.util.Scanner;
+
+/*
+ * 1 2 3   
+ * 4 5 6   
+ * 7 8 9   
+ * 
+ * arr[0]= {1,2,3}
+ * arr[1]= {4,5,6}
+ * arr[2]= {7,8,9}
+ * 
+ * 
+ * arr[0][0]=1
+ * arr[0][1]=2
+ * arr[0][2]=3 and so on...
+ */
+public class TwoDArray {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int[][] arr = new int[3][3];
+
+		for (int row = 0; row < arr.length; row++) {
+			for (int column = 0; column < arr[row].length; column++) {
+				arr[row][column] = scn.nextInt();
+			}
+		}
+
+		for (int row = 0; row < arr.length; row++) {
+			for (int column = 0; column < arr[row].length; column++) {
+				System.out.print(arr[row][column] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
