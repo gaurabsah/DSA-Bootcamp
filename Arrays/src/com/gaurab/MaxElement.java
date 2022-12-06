bb0fde77d39f4ace728c1632959a6ec43d7a6966
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-06 17:49:18 +0530

    Maximum Array and Range max

--- Arrays/src/com/gaurab/MaxElement.java
+++ Arrays/src/com/gaurab/MaxElement.java
@@ -3,6 +3,14 @@ package com.gaurab;
 public class MaxElement {
 
 	static int maxRange(int[] arr, int start, int end) {
+		if (end > start) {
+			return -1;
+		}
+
+		if (arr == null) {
+			return -1;
+		}
+		
 		int maxVal = arr[start];
 		for (int i = start; i <= end; i++) {
 			if (arr[i] > maxVal) {
@@ -13,6 +21,9 @@ public class MaxElement {
 	}
 
 	static int max(int[] arr) {
+		if (arr.length == 0) {
+			return -1;
+		}
 		int maxVal = arr[0];
 		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] > maxVal) {
