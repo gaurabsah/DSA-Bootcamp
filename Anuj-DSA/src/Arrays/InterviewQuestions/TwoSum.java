53a04726b99fc85e6c053275cc42779a9bb96434
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-01-05 13:29:44 +0530

    1. Two Sum

--- Anuj-DSA/src/Arrays/InterviewQuestions/TwoSum.java
+++ Anuj-DSA/src/Arrays/InterviewQuestions/TwoSum.java
@@ -7,31 +7,24 @@ import java.util.Map;
 public class TwoSum {
 
 	public static int[] twoSum(int[] nums, int target) {
-		int[] result = new int[2];
 
 		Map<Integer, Integer> map = new HashMap<>();
 
 		for (int i = 0; i < nums.length; i++) {
 
-			if (map.containsKey(nums[i])) {
-
-				result[0] = i;
-
-				result[1] = map.get(nums[i]);
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), i };
 			}
 
-			else {
-
-				map.put(target - nums[i], i);
-			}
+			map.put(nums[i], i);
 		}
-		return result;
+		throw new IllegalArgumentException("no match found!");
 
 	}
 
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 11, 15 };
-		int n = arr.length;
 		int target = 9;
 
 		int[] ans = twoSum(arr, target);
