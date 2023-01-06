736880347e49aa8f77b3d0dcd1f05189f249fc28
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-01-06 15:45:19 +0530

    Resizing an array

--- Anuj-DSA/src/Arrays/InterviewQuestions/ArrayResize.java
+++ Anuj-DSA/src/Arrays/InterviewQuestions/ArrayResize.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ArrayResize {
 
-	static void resize(int[] arr, int n, int capacity) {
+	static int[] resize(int[] arr, int n, int capacity) {
 //		create new array
 		int[] temp = new int[capacity];
 
@@ -12,18 +12,17 @@ public class ArrayResize {
 		for (int i = 0; i < n; i++) {
 			temp[i] = arr[i];
 		}
-		arr = temp;
-		System.out.println(Arrays.toString(arr));
-		System.out.println(arr.length);
+
+		return temp;
 	}
 
 	public static void main(String[] args) {
 		int[] arr = { 5, 9, 3, 10 };
 		int n = arr.length;
 		int capacity = n * 2; // double the array
-		resize(arr, n, capacity);
-//		System.out.println(Arrays.toString(arr));
-//		System.out.println(arr.length);
+		int[] result = resize(arr, n, capacity);
+		System.out.println(Arrays.toString(result));
+		System.out.println(result.length);
 	}
 
 }
